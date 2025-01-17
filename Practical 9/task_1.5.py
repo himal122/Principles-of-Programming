9b69da2b6399e4a1caab33dc05f2c66a66e24cf6
himalacharya122 <acharyahimal122@gmail.com>
2025-01-17 14:50:23 +0545

    Week 09 submission

---
+++ Practical 9/task_1.5.py
@@ -0,0 +1,28 @@
+class Shape:
+    
+ #constructor for Shape class
+ def __init__(self, colour='Black'):
+    self.__colour = colour
+    # self._colour = colour
+    print("Shape constructor is called")
+ 
+ def displayShape(self):
+    print("Shape is displaying")
+    
+ def getColour(self):
+    return self.colour
+
+class Rectangle(Shape):
+    
+ def __init__(self):
+    super().__init__()
+    print("Rectangle constructor is called")
+    
+ def displayRectangle(self):
+    print("Rectangle is displaying")
+    
+ def getRectangleColour(self):
+    return self.getColour
+
+rec = Rectangle()
+print(rec.getRectangleColour())
